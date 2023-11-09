38dcffb8b88e478e14ad30ac3616d198916d48be
Alexander <alexobidiegwu@gmail.com>
2023-11-09 22:45:40 +0100

    'Updated again'

--- src/AllKeys.js
+++ src/AllKeys.js
@@ -1,5 +1,5 @@
 export const AllKeys = {
     ipAddress:'http://192.168.100.12:19002',
-    verifyNINKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEyMTk1NywiZW52IjoidGVzdCIsImlhdCI6MTY1MTEyNzg0N30.v0Mk6Cm1OmrutJ868AjOorv9o90lMq_Bipv89hnlVrA',
+    verifyNINKey: '',
     verifyNINurl: 'https://vapi.verifyme.ng/v1'
 }
